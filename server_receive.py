281a30cbe81fcf42f113919e318df87ab23ae448
Roberto Bigazzi <bigassonr@gmail.com>
2019-12-02 17:12:40 +0100

    Remote Control Complete

--- server_receive.py
+++ server_receive.py
@@ -1,7 +1,12 @@
 import socket
 import sys
 import struct
- 
+import io
+import tkinter
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
+
 #HOST = '127.0.0.1' #this is your localhost
 #HOST = '172.20.10.5' #this is ip of Iphone Biga
 #HOST = '100.93.45.7' #this is ip of Fastweb
@@ -9,6 +14,10 @@ HOST = '192.168.43.155' #ip bigabyte
 PORT = 8888
  
 def start():
+    root = tkinter.Tk()
+    root.geometry('+%d+%d' % (100,100))
+    old_label_image = None
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #socket.socket: must use to create a socket.
     #socket.AF_INET: Address Format, Internet = IP Addresses.
@@ -27,12 +36,13 @@ def start():
     #listen(): This method sets up and start TCP listener.
     s.listen(5)
     print ("Receiving Socket is Now Listening")
-
-    while 1:
+    
+    while True:
         conn, addr = s.accept() 
         #print ('Connected with ' + addr[0] + ':' + str(addr[1]))
 
         buf = bytearray()
+        full_data = bytearray()
         while len(buf)<4:
             buf += conn.recv(4-len(buf))
         size = struct.unpack('!i', buf)[0]
@@ -40,11 +50,25 @@ def start():
         with open('/home/bigazzon/Downloads/test.jpg', 'wb') as f:
             while size > 0:
                 data = conn.recv(1024)
-                f.write(data)
+                #f.write(data)
                 size -= len(data)
+                full_data += data
         #print('Image Saved')
-        conn.close()
-
+        try:
+            image = Image.open(io.BytesIO(full_data))
+            root.geometry('%dx%d' % (image.size[0],image.size[1]))
+            tkpi = ImageTk.PhotoImage(image)
+            label_image = tkinter.Label(root, image=tkpi)
+            label_image.place(x=0,y=0,width=image.size[0],height=image.size[1])
+            root.title("Loomo Head Camera")
+            if old_label_image is not None:
+                old_label_image.destroy()
+            old_label_image = label_image
+            root.update_idletasks()
+            root.update()
+            #root.mainloop()
+        except Exception as e:
+            pass
 
         #buf = conn.recv(64)
         #print (buf)
--- server_send.py
+++ server_send.py
@@ -11,51 +11,65 @@ HOST = '192.168.43.155' #ip bigabyte
 PORT = 8889
  
 def start():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  #socket.socket: must use to create a socket.
-  #socket.AF_INET: Address Format, Internet = IP Addresses.
-  #socket.SOCK_STREAM: two-way, connection-based byte streams.
-  #print ("Sending Socket Created")
-   
-  #Bind socket to Host and Port
-  try:
-      s.bind((HOST, PORT))
-  except socket.error as err:
-      print ("Bind Failed, Error Code: " + str(err[0]) + ', Message: ' + str(err[1]))
-      sys.exit()
-   
-  #print ("Socket Bind Success!")
-   
-  #listen(): This method sets up and start TCP listener.
-  s.listen(10)
-  print ("Sending Socket is Now Listening")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #socket.socket: must use to create a socket.
+    #socket.AF_INET: Address Format, Internet = IP Addresses.
+    #socket.SOCK_STREAM: two-way, connection-based byte streams.
+    #print ("Sending Socket Created")
+     
+    #Bind socket to Host and Port
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as err:
+        print ("Bind Failed, Error Code: " + str(err[0]) + ', Message: ' + str(err[1]))
+        sys.exit()
+     
+    #print ("Socket Bind Success!")
+     
+    #listen(): This method sets up and start TCP listener.
+    s.listen(10)
+    print ("Sending Socket is Now Listening")
 
-  while 1:
-      conn, addr = s.accept() 
-      print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-      print ("Available actions:\n" + \
-             "1) Rotate Left\n" + \
-             "2) Rotate Right\n" + \
-             "3) Head Up\n" + \
-             "4) Head Down\n" + \
-             "5) Move Ahead\n" + \
-             "6) Reset Head")
-      while 1:
-          num = -1
-          while(num<1 or num>7):
-              num = int(input("Select your action: "))
-              if num == 0:
-                  print ("Available actions:\n" + \
-                         "1) Rotate Left\n" + \
-                         "2) Rotate Right\n" + \
-                         "3) Head Up\n" + \
-                         "4) Head Down\n" + \
-                         "5) Move Ahead\n" + \
-                         "6) Reset Head")
-          if not (num<1 or num>7):
-              msg = str(num)
-              #print(msg + " sending")
-              conn.send(msg.encode('utf-8'))
-              #msg = 1;
-              #conn.send(msg)
-  s.close()
+    while True:
+        conn, addr = s.accept() 
+        print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+        print ("Available actions:\n" + \
+               "1) Rotate Left\n" + \
+               "2) Rotate Right\n" + \
+               "3) Head Up\n" + \
+               "4) Head Down\n" + \
+               "5) Move Ahead\n" + \
+               "6) Reset Head\n" + \
+               "7) Speak")
+        while True:
+            num = -1
+            s = input("Select your action or enter string: ")
+            if (is_int(s)):
+                num = int(s)
+                if num == 0:
+                        print ("Available actions:\n" + \
+                               "1) Rotate Left\n" + \
+                               "2) Rotate Right\n" + \
+                               "3) Head Up\n" + \
+                               "4) Head Down\n" + \
+                               "5) Move Ahead\n" + \
+                               "6) Reset Head\n" + \
+                               "7) Speak")    
+                if not (num<1 or num>7):
+                    msg = str(num) + "\n"
+                    #print(msg + " sending")
+                    conn.send(msg.encode('utf-8'))
+                    #msg = 1;
+                    #conn.send(msg)
+            else:
+                msg = s + "\n"
+                conn.send(msg.encode('utf-8'))
+                #print(msg + " sending") 
+    s.close()
+
+def is_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
